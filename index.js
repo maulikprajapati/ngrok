35781857e40352cb35d8835dfd713fa55221258c
Maulik <maulikprajapati47@gmail.com>
2023-11-23 19:15:58 +0530

    fc

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+let url;
+const ngrok = require('ngrok');
+(async function () {
+    url = await ngrok.connect();
+    console.log('==========CONNECTED=================', url)
+})();
+
+
+const app = express();
+const PORT = process.env.PORT || 3030;
+
+// your code
+
+app.listen(PORT, () => {
+    console.log(`server started on port ${PORT}`);
+});
+
+app.get('/url', (req, res) => {
+    res.send(url);
+});
