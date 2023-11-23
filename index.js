633988cf8d40d30f639179bc4bd892b1f57e1527
Maulik <maulikprajapati47@gmail.com>
2023-11-23 20:43:09 +0530

    asdasd

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 let url;
-const ngrok = require('ngrok');
-(async function () {
-    url = await ngrok.connect();
-    console.log('==========CONNECTED=================', url)
-})();
+// const ngrok = require('ngrok');
+// (async function () {
+//     url = await ngrok.connect();
+//     console.log('==========CONNECTED=================', url)
+// })();
 
 
 
